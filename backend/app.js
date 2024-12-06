6b6ae84a94588acc73af5ac12ddb7f59b2962ec0
Aadarsh Soni <84452674+aadarsh-2004@users.noreply.github.com>
2024-12-06 23:05:27 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -66,7 +66,7 @@ app.post("/ask", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
